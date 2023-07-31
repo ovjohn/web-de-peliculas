1884bc254878c75540f3252d743fb372070f335c
“John” <olivaresvjohn@gmail.com>
2023-07-31 18:53:11 -0400

    Creando el componente MoviesCard

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,12 +1,12 @@
 import peliculas from "../data/movies.json";
+import { MovieCard } from "./MovieCard";
 
 export function MoviesGrid(){
     return(
         <ul>
             {peliculas.map( pelicula =>
-                <li key={pelicula.id}>
-                    {pelicula.title}
-                </li>)}
+                <MovieCard key={pelicula.id} movie={pelicula} />
+            )}
         </ul>
     )
 }
